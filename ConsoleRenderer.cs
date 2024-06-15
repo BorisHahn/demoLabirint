410942a2a85de28a127f2eca12d6ee3479a54763
Boris Gaidukevich <Borishahn@yandex.ru>
2024-06-15 21:38:13 +0300

    0.0.1

---
+++ ConsoleRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoLabirint
+{
+    public class ConsoleRenderer
+    {
+        private char[,] _pixels;
+        private char[,] _previousPixels;
+        private int _width;
+        private int _height;
+
+        public ConsoleRenderer()
+        {
+            _width = Console.WindowWidth;
+            _height = Console.WindowHeight;
+            _pixels = new char[_width, _height];
+            _previousPixels = new char[_width, _height];
+            Console.CursorVisible = false;
+        }
+
+        public void SetPixel(int w, int h, char val)
+        {
+            _pixels[w, h] = val;
+        }
+
+        public void Render()
+        {
+            for (int w = 0; w < _width; w++)
+            {
+                for (int h = 0; h < _height; h++)
+                {
+                    if (_previousPixels[w, h] == _pixels[w, h])
+                        continue;
+                    Console.SetCursorPosition(w, h);
+                    Console.Write(_pixels[w, h]);
+                    _previousPixels[w, h] = _pixels[w, h];
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace demoLabirint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] map = new[,]
+            {
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',},
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#',},
+            };
+
+            int playerX = 1;
+            int playerY = 1;
+            ConsoleRenderer renderer = new ConsoleRenderer();
+
+            SetMapPixels(map, renderer);
+            SetPlayer(playerX, playerY, renderer);
+
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                renderer.Render();
+            }
+
+        }
+
+        static void SetMapPixels(char[,] map, ConsoleRenderer renderer)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    renderer.SetPixel(i, j, map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void SetPlayer(int x, int y, ConsoleRenderer renderer)
+        {
+            renderer.SetPixel(x, y, '@');
+        }
+    }
+}
